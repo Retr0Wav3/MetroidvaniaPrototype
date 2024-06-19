211fcc6a0b5399a1c9e649ef472c2e810f13dc00
Anatoly Vasilyev <xillead@gmail.com>
2024-06-20 00:23:53 +0300

    set up player movement via new input system

---
+++ Assets/Scripts/Input/MovementController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Scripts.Input
+{
+    public class MovementController : MonoBehaviour
+    {
+        private Player _player;
+
+        private void Awake()
+        {
+            TryGetComponent(out _player);
+        }
+
+        public void OnHorizontalMovement(InputAction.CallbackContext context)
+        {
+            var direction = context.ReadValue<float>();
+            _player.SetMovementDirection(direction);
+        }
+    }
+}
---
+++ Assets/Scripts/Input/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Scripts.Input
+{
+    public class Player : MonoBehaviour
+    {
+        [SerializeField] private float _speed;
+        private Vector3 _directionVector;
+        
+        public void SetMovementDirection(float direction)
+        {
+            _directionVector.x = direction;
+        }
+
+        private void Update()
+        {
+            if (_directionVector.x != 0)
+                transform.position += _directionVector * (_speed * Time.deltaTime);
+        }
+    }
+}
